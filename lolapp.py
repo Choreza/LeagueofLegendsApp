206a03f4b487575212b958b2f066951cc1903790
gnorambuena <gnorambuena@users.noreply.github.com>
2017-06-08 20:48:06 -0300

    modified table

--- lolapp.py
+++ lolapp.py
@@ -9,7 +9,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>gabriel ql!</h1>"
+    return render_template("request1.html")
 
 @application.route("/test/<name>")
 def hola(name):
