375f854f7a0327bc34b9c0d53e8f8658efcdc3aa
gnorambuena <gnorambuena@users.noreply.github.com>
2017-06-08 22:03:22 -0300

    post method

--- lolapp.py
+++ lolapp.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request, redirect, url_for
 from parser import Parser
 from SQLWrapper import SQLWrapper
@@ -7,18 +6,21 @@ parser = Parser()
 SQL = SQLWrapper()
 application = Flask(__name__)
 
+
 @application.route("/")
 def hello():
     return render_template("request1.html")
+
 
 @application.route("/test/<name>")
 def hola(name):
     SQL.query1(name)
     data = parser.tableheader(SQL.colnames)
     data += parser.tableBody(SQL.fetch())
-    return render_template("test.html",data=data)
+    return render_template("test.html", data=data)
 
-@application.route("/grupo07/handle_data",methods=['GET','POST'])
+
+@application.route("/grupo07/handle_data", methods=['GET', 'POST'])
 def handle_data():
     name = request.form['projectFilepath']
     print request.method
