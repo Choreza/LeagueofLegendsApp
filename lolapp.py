81f3f8debd1843d05a257d091cf1a1a35661bdde
choreza <aege21@gmail.com>
2017-06-11 13:37:26 -0400

    fixed error in lolapp.py

--- lolapp.py
+++ lolapp.py
@@ -9,7 +9,7 @@ home = "http://cc3201.dcc.uchile.cl/grupo07"
 
 @application.route("/")
 def hello():
-    return url_for("index.html")
+    return render_template("index.html")
 
 
 @application.route("/test/<name>")
