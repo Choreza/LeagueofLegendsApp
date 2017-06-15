f7c209dde3873722359f8a726cfa553a66098696
gnorambuena <gnorambuena@users.noreply.github.com>
2017-06-14 23:56:25 -0300

    More fancy

--- SQLWrapper.py
+++ SQLWrapper.py
@@ -41,7 +41,7 @@ class SQLWrapper:
             except:
                 pass
             self.cur.execute("SELECT foo.season,foo.wins,bar.total, CAST(foo.wins AS double precision) / bar.total AS winrate FROM (SELECT season,COUNT(season) AS wins FROM lol.leagueoflegends WHERE ((%s) IN (redtopchamp,redjunglechamp,redmiddlechamp,redadcchamp,redsupportchamp) AND rresult IS true) OR ((%s) IN (bluetopchamp,bluejunglechamp,bluemiddlechamp,blueadcchamp,bluesupportchamp) AND bresult IS true) GROUP BY season) foo, (SELECT season,COUNT(season) AS total FROM lol.leagueoflegends WHERE (%s) IN (redtopchamp,redjunglechamp,redmiddlechamp,redadcchamp,redsupportchamp,bluetopchamp,bluejunglechamp,bluemiddlechamp,blueadcchamp,bluesupportchamp) GROUP BY (season) ) bar WHERE foo.season = bar.season", (data,data,data))
-            self.colnames = [desc[0] for desc in self.cur.description]
+            self.colnames = [desc[0].capitalize() for desc in self.cur.description]
         except Exception,e:
             print str(e)
             print "Can't execute query"
