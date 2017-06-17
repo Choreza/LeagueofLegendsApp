b6bc613f338e73024b473a26b1a1ae919473331f
choreza <aege21@gmail.com>
2017-06-17 18:44:41 -0400

    edited sqlwrapper

--- SQLWrapper.py
+++ SQLWrapper.py
@@ -64,7 +64,7 @@ class SQLWrapper:
                 self.cur.fetchall()
             except:
                 pass
-            self.cur.execute("SELECT blueteamtag AS "Blue Team", redteamtag AS "Red Team", bresult, rresult, TO_CHAR((gamelength || ' minute')::interval, 'HH24:MI') AS "Duración", year AS "Año", season AS "Temporada" FROM lol.leagueoflegends ORDER BY YEAR ASC;",(data,data,data))
+            self.cur.execute("SELECT blueteamtag AS "Blue Team", redteamtag AS "Red Team", bresult, rresult, TO_CHAR((gamelength || ' minute')::interval, 'HH24:MI') AS "Duracion", year AS "Ano", season AS "Temporada" FROM lol.leagueoflegends ORDER BY YEAR ASC;",(data,data,data))
             self.colnames = [desc[0].capitalize() for desc in self.cur.description]
         except Exception,e:
             print str(e)
