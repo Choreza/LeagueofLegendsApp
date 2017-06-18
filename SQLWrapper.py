2f4a6dfa1f6d31559fcdbde937081d65e8319178
choreza <aege21@gmail.com>
2017-06-18 00:12:33 -0400

    updated sqlwrapper

--- SQLWrapper.py
+++ SQLWrapper.py
@@ -58,6 +58,15 @@ class SQLWrapper:
             print str(e)
             print "Can't execute query"
 
+   	def queryMatchBySeason(self, data):
+   		try:
+   			try:
+   				self.cur.fetchall()
+   			except:
+   				pass
+   			self.cur.execute("SELECT blueteamtag AS "+"Blue Team"+", redteamtag AS "+"Red Team"+", bresult, rresult, TO_CHAR((gamelength || ' minute')::interval, 'HH24:MI') AS "+"Duración"+", year AS "+"Año"+", season AS "+"Temporada"+" FROM lol.leagueoflegends ORDER BY YEAR ASC;")
+   			self.colnames = [desc[0].capitalize() for desc in self.cur.description]
+
     #fetch los siguientes 100 resultados
     def fetch(self):
         try:
