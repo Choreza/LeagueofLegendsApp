5cec6c09a001237b79bc1207c1b2c7777f04de0f
root <root@ahogan-cc3201-07>
2017-06-18 07:42:33 -0400

    finally fixed indentation errors

--- SQLWrapper.py
+++ SQLWrapper.py
@@ -49,20 +49,20 @@ class SQLWrapper:
             print "Can't execute query"
     
     def queryMatchBySeason(self):
-   	    try:
-   		    try:
-   			    self.cur.fetchall() 
+   	try:
+ 	    try:
+		self.cur.fetchall() 
             except:
-   			    pass
-   		self.cur.execute("SELECT blueteamtag AS "+"""Blue Team"""+", redteamtag AS "+"""Red Team"""+", bresult, rresult, TO_CHAR((gamelength || ' minute')::interval, 'HH24:MI') AS "+"""Duración"""+", year AS "+"""Año"""+", season AS "+"""Temporada"""+" FROM lol.leagueoflegends ORDER BY YEAR ASC")
-   		self.colnames = [desc[0].capitalize() for desc in self.cur.description]
-   		return self.colnames
+   	        pass
+   	    self.cur.execute("SELECT blueteamtag AS "+"""Blue Team"""+", redteamtag AS "+"""Red Team"""+", bresult, rresult, TO_CHAR((gamelength || ' minute')::interval, 'HH24:MI') AS "+"""Duración"""+", year AS "+"""Año"""+", season AS "+"""Temporada"""+" FROM lol.leagueoflegends ORDER BY YEAR ASC")
+   	    self.colnames = [desc[0].capitalize() for desc in self.cur.description]
+   	    return self.colnames
    	except Exception, e:
-   		print str(e)
-   		print "Can't execute query"
+   	    print str(e)
+   	    print "Can't execute query"
         
     def queryChampionYear(self,data):
-   	    try:
+  	try:
             try:
                 self.cur.fetchall()
             except:
