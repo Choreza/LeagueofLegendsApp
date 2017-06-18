539a2d8c8ea30bde5a9e5e7d32b7f2f0caf386a8
gnorambuena <gnorambuena@users.noreply.github.com>
2017-06-18 13:20:46 -0300

    modified season page

--- SQLWrapper.py
+++ SQLWrapper.py
@@ -57,7 +57,7 @@ class SQLWrapper:
             except:
                 pass
             self.cur.execute(
-                "SELECT blueteamtag AS BlueTeam, redteamtag AS RedTeam, bresult, rresult, TO_CHAR((gamelength || ' minute')::interval, 'HH24:MI') AS Duración, year AS Año, season AS Temporada FROM lol.leagueoflegends WHERE season = (%s) ORDER BY año %s",
+                "SELECT blueteamtag AS BlueTeam, redteamtag AS RedTeam, bresult, rresult, TO_CHAR((gamelength || ' minute')::interval, 'HH24:MI') AS Duración, year AS Año, season AS Temporada FROM lol.leagueoflegends WHERE season = (%s) ORDER BY año (%s)",
                 (season, order))
             self.colnames = [desc[0].capitalize() for desc in self.cur.description]
         except Exception, e:
